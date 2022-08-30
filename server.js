5d05ff658ea966a51c24a8412b408a2db0545f30
Ryan Raposo <raposo.ryan@gmail.com>
2022-08-30 15:16:11 -0400

    add db connection to server + env file

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.MYSQL_PASSWORD,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 // Default response
 app.use((req, res) => {
     res.status(404).end();
-})
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
